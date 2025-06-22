9b5173c185453abd1ff7ecb9e80a78c985ca36fa
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-06-22 14:10:02 +0700

    ADDED ENDPOINT

---
+++ src/utils/Endpoint.jsx
@@ -0,0 +1,5 @@
+export const BASEURL = "http://localhost:4000";
+
+export const URL_SIGIN = `${BASEURL}/api/auth/signin`;
+export const URL_PRODUCTS = `${BASEURL}/api/products`;
+export const URL_TRANSACTIONS = `${BASEURL}/api/transactions`;
