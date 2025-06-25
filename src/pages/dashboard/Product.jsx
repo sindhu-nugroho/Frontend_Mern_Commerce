4a6178393fe629b408f3055a2425e10b668be98a
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-06-25 11:06:28 +0700

    change the display of dashboard product and re-make the delete button

--- src/pages/dashboard/Product.jsx
+++ src/pages/dashboard/Product.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Table, Button, Image } from "antd";
+import { Table, Button, Image, Popconfirm, message } from "antd";
 import axios from "axios";
 import {URL_PRODUCTS} from "../../utils/Endpoint";
 import { data, Link } from "react-router-dom";
@@ -8,6 +8,17 @@ import { use } from "react";
 const Product = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
+
+    // added function to delete product
+    const handleDelete = async (id) => {
+        try {
+            await axios.delete(`${URL_PRODUCTS}/${id}`);
+            message.success('product deleted succesfully');
+            setProducts(products.filter((item) => item._id !== id));
+        } catch (err) {
+            message.error("Failed to delete product");
+        }
+    }
 
     useEffect(() => {
         setLoading(true);
@@ -46,36 +57,41 @@ const Product = () => {
             key: 'price',
         },
         {
-            title: 'Action',
-            key: 'action',
+            title: 'Update',
+            key: 'update',
             render: (_, record) => (
                 <>
                     <Button>
                         <Link to={`/dashboard/products/${record?._id}`}>Update</Link>
                     </Button>
-                    <Button
-                        type='primary'
-                        danger
-                        onClick={() => {
-                            console.log('id', record?._id);
-                            axios
-                                .delete(`${URL_PRODUCT}/${record?._id}`)
-                                .then((res) => {
-                                    console.log(res);
-                                    window.location.reload();
-                                })
-                                .catch((err) => console.log("err", err.response));
-                        }}>
-                        Delete
-                    </Button>
                 </>
             ),
         },
+        {
+            title: 'Delete',
+            key: 'delete',
+            render: (_, record) => (
+                <Popconfirm
+                    title="Are you sure to delete this product"
+                    onConfirm={() => handleDelete(record._id)}
+                    okText='Yes'
+                    cancelText='No'
+                >
+                    <Button danger type='primary'>Delete</Button>
+                </Popconfirm>
+            )
+        }
     ];
 
     return (
         <div>
             <h1>List Product</h1>
+            <Table
+                dataSource={products}    
+                columns={columns}
+                rowKey="_id"
+                loading={loading}
+            />
             <Link to='/dashboard/products/create'>
                 <Button type='primary'>Tambah</Button>
             </Link>
