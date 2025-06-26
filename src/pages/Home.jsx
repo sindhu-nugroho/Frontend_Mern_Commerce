ee14c5433a7327a8927648ad1eda5f174e5dd78c
FateGreedtony <amalikfajarmusyafa2901@gmail.com>
2025-06-26 09:08:25 +0700

    Make the home display more friendly when there is no product to display

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,27 +32,48 @@ const Home = () => {
     return (
         <div style={{ padding: '20px'}}>
             <Title level={2}>Produk List</Title>
-            <Row gutter={[16, 16]}>
-                {products.map((product) => (
-                    <Col span={8} key={product.id}>
-                        <Card
-                            hoverable
-                            cover={<img alt={product.name} src={product.thumbnail} />}>                  
-                                <Card.Meta
-                                    title={product.name}
-                                    description={`Rp ${product.price}`} 
-                                />  
-                                <Button
-                                    type="primary"
-                                    icon={<ShoppingCartOutlined />}
-                                    style={{ marginTop: '10px' }}
-                                >
-                                    <Link to={`/checkout/${product._id}`}>CHeckout Now</Link>
-                                </Button>     
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
+            {products.length === 0 ? (
+                <div style={{ 
+                    textAlign: 'center', 
+                    marginTop: 60, 
+                    color: '#888' 
+                }}>
+                    <img 
+                        src="https://cdn-icons-png.flaticon.com/512/4076/4076549.png" 
+                        alt="No Product" 
+                        width={120} 
+                        style={{ opacity: 0.5, marginBottom: 20 }}
+                    />
+                    <div style={{ fontSize: 22, fontWeight: 500, marginBottom: 8 }}>
+                        Tidak ada produk yang tersedia
+                    </div>
+                    <div style={{ fontSize: 16 }}>
+                        Silakan kembali lagi nanti atau hubungi admin untuk info lebih lanjut.
+                    </div>
+                </div>
+            ) : (
+                <Row gutter={[16, 16]}>
+                    {products.map((product) => (
+                        <Col span={8} key={product.id}>
+                            <Card
+                                hoverable
+                                cover={<img alt={product.name} src={product.thumbnail} />}>                  
+                                    <Card.Meta
+                                        title={product.name}
+                                        description={`Rp ${product.price}`} 
+                                    />  
+                                    <Button
+                                        type="primary"
+                                        icon={<ShoppingCartOutlined />}
+                                        style={{ marginTop: '10px' }}
+                                    >
+                                        <Link to={`/checkout/${product._id}`}>CHeckout Now</Link>
+                                    </Button>     
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            )}
         </div>
     );
 };
